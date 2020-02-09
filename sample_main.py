680db6d3a759f5b79856372fed5bdd1e64f9aeeb
manoj <mnjkmr23696@gmail.com>
2020-02-10 01:09:37 +0530

    added six1

--- sample_main.py
+++ sample_main.py
@@ -1,12 +1,6 @@
-hungry=input("are you hungry")
-if hungry=="yes":
-	print("eat samosa")
-	print("eat burger")
-	print("eat pizza")
-	print("adding change in master")
-	print("ram")
-else:
-    print("bhaj hare krishn")
-	print("ram")
-	print("krishn")
-	print("radha")
+print ("branch three")
+print ("branch three")
+print ("branch one")
+print ("branch two")
+print(branch six)
+print ("branch two")
